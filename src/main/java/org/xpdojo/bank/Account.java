22b04ff08e3d901a404d65c370e2f143c3768b32
jasmin.tailor@db.com <J@smin27>
2022-01-28 18:23:25 +0530

    removing unused code

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -3,10 +3,6 @@ package org.xpdojo.bank;
 public class Account {
     int bal = 0;
 
-    public void deposit(int i) {
-
-    }
-
     public int balance() {
         return bal;
     }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -6,18 +6,10 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
-
-    //@Test
-	//@Disabled
-    /*public void depositAnAmountToIncreaseTheBalance() {
-
-        assertThat("your first test").isBlank();
-    }*/
-
+    
     @Test
     public void initialAccount_shouldHaveZeroBalance(){
         Account account = new Account();
-        account.deposit(100);
         assertThat(account.balance()).isEqualTo(0);
     }
 
